2e2189f2f64ba6d2b33b03744d2a04aac553c251
Dante1704 <dante.nkaddarian@gmail.com>
2023-11-30 17:30:41 -0300

    creada conexion a db de mysql y mostrado primer resultado de consulta

--- demo-5/index.php
+++ demo-5/index.php
@@ -3,7 +3,22 @@
 <?php
 
 require'functions.php';
-require'router.php';
+/* require'router.php'; */
 
 
-/* dd($_SERVER); */
+
+/* string connection to the db */
+$dsn = "mysql:host=localhost;port=3306;dbname=demo5;user=root;charset=utf8mb4";
+
+/* nueva conexion a la db */
+$pdo = new PDO($dsn);
+
+/* preparacion y ejecucion de la query */
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC); /*PDO::FETCH_ASSOC para que me muestre la respuesta en forma de assoc array*/
+
+foreach ($posts as $post) {
+   echo "<li>" . $post['title'] . "</li>";
+}
