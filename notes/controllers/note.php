15570227d8cf07515282df0093f633f922583e5d
Dante1704 <dante.nkaddarian@gmail.com>
2023-12-01 18:40:27 -0300

    se creo una class Response para los codigos de respuesta evitando magic numbers

--- notes/controllers/note.php
+++ notes/controllers/note.php
@@ -9,7 +9,6 @@ $heading = 'Note';
 
 $id = $_GET['id'];
 
-
 //esta query tiene el problema de que puedo estar trayendo notas que no son del current user
 $query = "select * from notes where id = :id";
 
@@ -20,10 +19,12 @@ if(! $note) {
     abort(); //por defecto es el 404 y en este caso esta bien
 } 
 
+$currentUserId = 3;
+
 //aca tengo en cuenta si la nota le pertenece al current user
 //en este caso estoy hardcodeando el user_id porque todavia no tengo hecha la authentication
-if($note['user_id'] !== 3) {
-    abort(403); //forbidden
+if($note['user_id'] !== $currentUserId) {
+    abort(Response::FORBIDDEN); //lo traigo de la class Response donde tengo representados los codigos de respuestas
 }
 /* dd($note); */
 
